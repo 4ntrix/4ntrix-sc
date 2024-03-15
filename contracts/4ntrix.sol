8f7e8b93eea56cab8dc1aa9ce2b185423b73789b
Ojas Aklecha <ojasaklechayt@gmail.com>
2024-03-16 00:31:38 +0530

    Deployed the contract V2

--- contracts/4ntrix.sol
+++ contracts/4ntrix.sol
@@ -6,11 +6,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Antrix is ERC721, Ownable {
     uint256 public totalOccasions;
-    uint256 public totalSupply;
-    uint256 public constant MAX_TICKETS_PER_ADDRESS = 5;
+    uint256 public totalTickets; // Track total tickets minted
 
     struct Occasion {
-        uint256 id;
         string name;
         uint256 cost;
         uint256 tickets;
@@ -29,24 +27,35 @@ contract Antrix is ERC721, Ownable {
 
     mapping(uint256 => Occasion) public occasions;
     mapping(uint256 => mapping(address => bool)) public hasBought;
-    mapping(uint256 => mapping(uint256 => address)) public seatTaken;
-    mapping(uint256 => uint256[]) public seatsTaken;
     mapping(address => User) public users;
 
-    event OccasionListed(uint256 indexed id, string name, uint256 cost, uint256 maxTickets, string date, string time, string location, string ipfsImageHash);
-    event TicketsPurchased(address indexed buyer, uint256 indexed occasionId, uint256 ticketId);
-    event AttendanceMarked(address indexed attendee, uint256 indexed occasionId);
+    event OccasionListed(
+        uint256 indexed id,
+        string name,
+        uint256 cost,
+        uint256 maxTickets,
+        string date,
+        string time,
+        string location,
+        string ipfsImageHash
+    );
+    event TicketsPurchased(address indexed buyer, uint256 indexed occasionId);
+    event AttendanceMarked(
+        address indexed attendee,
+        uint256 indexed occasionId
+    );
 
     modifier onlyAdminOrOwner() {
-        require(owner() == msg.sender || admins[msg.sender], "Caller is not an admin or owner");
+        require(
+            owner() == msg.sender || admins[msg.sender],
+            "Caller is not an admin or owner"
+        );
         _;
     }
 
     mapping(address => bool) public admins;
 
-    constructor(
-        address _owner
-    ) ERC721("Antrix", "ANT") Ownable(_owner) {
+    constructor(address _owner) ERC721("Antrix", "ANT") Ownable(_owner) {
         admins[_owner] = true;
     }
 
@@ -70,7 +79,6 @@ contract Antrix is ERC721, Ownable {
     ) public onlyAdminOrOwner {
         totalOccasions++;
         occasions[totalOccasions] = Occasion(
-            totalOccasions,
             _name,
             _cost,
             _maxTickets,
@@ -81,32 +89,39 @@ contract Antrix is ERC721, Ownable {
             _ipfsImageHash
         );
 
-        emit OccasionListed(totalOccasions, _name, _cost, _maxTickets, _date, _time, _location, _ipfsImageHash);
+        emit OccasionListed(
+            totalOccasions,
+            _name,
+            _cost,
+            _maxTickets,
+            _date,
+            _time,
+            _location,
+            _ipfsImageHash
+        );
     }
 
-    function mint(uint256 _id, uint256 _seat, uint256 _ticketCost) public payable {
+    function mint(uint256 _id, uint256 _ticketCost) public payable {
         require(_id != 0, "Invalid occasion ID");
         require(_id <= totalOccasions, "Occasion does not exist");
 
         Occasion storage occasion = occasions[_id];
         require(occasion.tickets > 0, "Tickets sold out");
-        require(occasion.tickets >= _seat, "Invalid seat");
 
-        require(getNumTicketsPurchased(msg.sender) + 1 <= MAX_TICKETS_PER_ADDRESS, "Exceeded maximum tickets per address");
+        require(
+            getNumTicketsPurchased(msg.sender) + 1 <= occasion.maxTickets,
+            "Exceeded maximum tickets for this occasion"
+        );
         require(occasion.cost > 0, "Ticket cost not set");
-
-        require(seatTaken[_id][_seat] == address(0), "Seat already taken");
 
         // Check if user sent enough ether for the ticket
         require(msg.value >= _ticketCost, "Insufficient payment");
 
         occasions[_id].tickets--;
         hasBought[_id][msg.sender] = true;
-        seatTaken[_id][_seat] = msg.sender;
-        seatsTaken[_id].push(_seat);
-        totalSupply++;
+        totalTickets++; // Increment total tickets minted
 
-        _safeMint(msg.sender, totalSupply);
+        _safeMint(msg.sender, totalTickets);
 
         // Refund excess payment
         uint256 excessPayment = msg.value - _ticketCost;
@@ -114,14 +129,19 @@ contract Antrix is ERC721, Ownable {
             payable(msg.sender).transfer(excessPayment);
         }
 
-        emit TicketsPurchased(msg.sender, _id, totalSupply);
+        emit TicketsPurchased(msg.sender, _id);
     }
 
-
-    function markAttendance(uint256 _id, address _attendee) public onlyAdminOrOwner {
+    function markAttendance(uint256 _id, address _attendee)
+        public
+        onlyAdminOrOwner
+    {
         require(_id != 0, "Invalid occasion ID");
         require(_id <= totalOccasions, "Occasion does not exist");
-        require(hasBought[_id][_attendee], "Address has not bought a ticket for this occasion");
+        require(
+            hasBought[_id][_attendee],
+            "Address has not bought a ticket for this occasion"
+        );
 
         hasBought[_id][_attendee] = true;
 
@@ -144,7 +164,11 @@ contract Antrix is ERC721, Ownable {
 
         for (uint256 i = 1; i <= totalOccasions; i++) {
             if (i != _id) {
-                require(keccak256(abi.encodePacked(occasions[i].name)) != keccak256(abi.encodePacked(_name)), "Cannot set occasion details to existing occasion's name");
+                require(
+                    keccak256(abi.encodePacked(occasions[i].name)) !=
+                        keccak256(abi.encodePacked(_name)),
+                    "Cannot set occasion details to existing occasion's name"
+                );
             }
         }
 
@@ -167,7 +191,11 @@ contract Antrix is ERC721, Ownable {
         admins[newOwner] = true;
     }
 
-    function getNumTicketsPurchased(address _address) public view returns (uint256) {
+    function getNumTicketsPurchased(address _address)
+        public
+        view
+        returns (uint256)
+    {
         uint256 numTickets = 0;
         for (uint256 i = 1; i <= totalOccasions; i++) {
             if (hasBought[i][_address]) {
@@ -181,11 +209,11 @@ contract Antrix is ERC721, Ownable {
         return occasions[_id];
     }
 
-    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
-        return seatsTaken[_id];
-    }
-
-    function addUser(string memory _name, string memory _aadharCard, string memory _ipfsImageHash) public {
+    function addUser(
+        string memory _name,
+        string memory _aadharCard,
+        string memory _ipfsImageHash
+    ) public {
         users[msg.sender] = User(_name, _aadharCard, _ipfsImageHash);
     }
 
@@ -201,11 +229,51 @@ contract Antrix is ERC721, Ownable {
         return allOccasions;
     }
 
-    function getAllUsers() public view returns (User[] memory) {
-        User[] memory allUsers = new User[](totalSupply);
-        for (uint256 i = 1; i <= totalSupply; i++) {
-            allUsers[i - 1] = users[ownerOf(i)];
+    function getTicketsByAddress(address _address)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory tickets = new uint256[](totalTickets);
+        uint256 ticketCount = 0;
+
+        for (uint256 i = 1; i <= totalOccasions; i++) {
+            if (hasBought[i][_address]) {
+                tickets[ticketCount] = i;
+                ticketCount++;
+            }
         }
-        return allUsers;
+
+        uint256[] memory result = new uint256[](ticketCount);
+        for (uint256 j = 0; j < ticketCount; j++) {
+            result[j] = tickets[j];
+        }
+
+        return result;
+    }
+
+    function getUsersForOccasion(uint256 _occasionId)
+        public
+        view
+        returns (address[] memory)
+    {
+        address[] memory usersForOccasion = new address[](totalTickets);
+        uint256 userCount = 0;
+
+        for (uint256 i = 1; i <= totalTickets; i++) {
+            if (
+                ownerOf(i) != address(0) && hasBought[_occasionId][ownerOf(i)]
+            ) {
+                usersForOccasion[userCount] = ownerOf(i);
+                userCount++;
+            }
+        }
+
+        address[] memory result = new address[](userCount);
+        for (uint256 j = 0; j < userCount; j++) {
+            result[j] = usersForOccasion[j];
+        }
+
+        return result;
     }
 }
